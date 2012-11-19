e165143ff629c193a2e9c31874b66c0925de3c60
Mike Boers <mboers@keypics.com>
2012-11-19 11:38:32 -0800

    Add more defaults to Maya exporter

--- sgpublish/io/maya.py
+++ sgpublish/io/maya.py
@@ -1,5 +1,7 @@
 from __future__ import absolute_import
 
+import os
+
 from . import base
 
 from maya import cmds
@@ -8,6 +10,17 @@ __also_reload__ = ['.base']
 
 
 class Exporter(base.Exporter):
+    
+    def __init__(self, *args, **kwargs):
+        super(Exporter, self).__init__(*args, **kwargs)
+    
+    @property
+    def filename_hint(self):
+        return self._filename_hint or cmds.file(q=True, sceneName=True) or None
+    
+    @property
+    def workspace(self):
+        return self._workspace or cmds.workspace(query=True, rootDirectory=True) or os.getcwd()
     
     def get_previous_publish_ids(self):
         ids = cmds.fileInfo('sgpublish_%s_ids' % self.publish_type, query=True)
