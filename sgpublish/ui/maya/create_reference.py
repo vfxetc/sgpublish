bfbf0753f58ba9499f382ac6be717cd0c6ccc242
Mike Boers <mboers@keypics.com>
2012-11-19 12:22:53 -0800

    Preview includes user and time

--- sgpublish/ui/maya/create_reference.py
+++ sgpublish/ui/maya/create_reference.py
@@ -1,4 +1,5 @@
 import functools
+import os
 
 from PyQt4 import QtCore, QtGui
 Qt = QtCore.Qt
@@ -6,11 +7,59 @@ Qt = QtCore.Qt
 from maya import cmds
 
 from sgfs.ui.picker import presets as picker_presets
+from sgfs import SGFS
 
 __also_reload__ = [
     'sgfs.ui.picker.presets',
 ]
 
+
+class Preview(QtGui.QWidget):
+    
+    def __init__(self):
+        super(Preview, self).__init__()
+        self._pixmaps = {}
+        self._setup_ui()
+    
+    def _setup_ui(self):
+        
+        self.setLayout(QtGui.QVBoxLayout())
+        
+        self._thumbnail = QtGui.QLabel('')
+        self._thumbnail.setFrameShape(QtGui.QFrame.StyledPanel)
+        self._thumbnail.setFrameShadow(QtGui.QFrame.Raised)
+        self.layout().addWidget(self._thumbnail)
+        
+        form = QtGui.QFormLayout()
+        self.layout().addLayout(form)
+        
+        self._created_by_label = QtGui.QLabel()
+        form.addRow("<b>By:</b>", self._created_by_label)
+        self._created_at_label = QtGui.QLabel()
+        form.addRow("<b>At:</b>", self._created_at_label)
+        
+        self.layout().addStretch()
+    
+    def update(self, entity):
+        
+        # Do this async.
+        by, at = entity.fetch(('created_by.HumanUser.name', 'created_at'))
+        self._created_by_label.setText(str(by))
+        self._created_at_label.setText(str(at.strftime('%y-%m-%d %I:%M %p')))
+        
+        if entity not in self._pixmaps:
+            path = os.path.join(SGFS(session=entity.session).path_for_entity(entity), '.sgpublish.thumbnail.jpg')
+            if os.path.exists(path):
+                pixmap = QtGui.QPixmap(path)
+            else:
+                path = os.path.abspath(os.path.join(
+                    __file__, '..', '..', '..', '..', 'art', 'no-thumbnail.png'
+                ))
+                pixmap = QtGui.QPixmap(path)
+            self._pixmaps[entity] = pixmap.scaledToWidth(165, Qt.SmoothTransformation)
+        
+        self._thumbnail.setPixmap(self._pixmaps[entity])
+        self._thumbnail.setFixedSize(self._pixmaps[entity].size())
 
 class Dialog(QtGui.QDialog):
     
@@ -38,8 +87,7 @@ class Dialog(QtGui.QDialog):
         self._button.clicked.connect(self._on_create_reference)
         button_layout.addWidget(self._button)
         
-        self._preview = QtGui.QLabel('preview')
-        self._preview_images = {}
+        self._preview = Preview()
         self._picker.setPreviewWidget(self._preview)
         self._picker.updatePreviewWidget.connect(self._on_update_preview)
     
@@ -50,13 +98,7 @@ class Dialog(QtGui.QDialog):
     def _on_update_preview(self, index):
         node = self._model.node_from_index(index)
         entity = node.state['PublishEvent']
-        if entity not in self._preview_images:
-            pixmap = QtGui.QPixmap()
-            from urllib import urlopen
-            pixmap.loadFromData(urlopen(entity.fetch('image')).read())
-            self._preview_images[entity] = pixmap.scaledToWidth(200, Qt.SmoothTransformation)
-        self._preview.setPixmap(self._preview_images[entity])
-        self._preview.setFixedHeight(self._preview_images[entity].height())
+        self._preview.update(entity)
         
     def _on_create_reference(self):
         publish = self._node.state['PublishEvent']
