36ec7beb00afc9badb28e5673dec2f0fa0648170
Mike Boers <mboers@keypics.com>
2012-11-22 17:24:53 -0800

    Start on playblast button

--- sgpublish/ui/exporter/maya/publish.py
+++ sgpublish/ui/exporter/maya/publish.py
@@ -1,5 +1,7 @@
+import contextlib
 import os
 import tempfile
+import subprocess
 
 from PyQt4 import QtGui
 
@@ -11,6 +13,83 @@ from ... import utils as ui_utils
 __also_reload__ = ['...utils', '..publish']
 
 
+@contextlib.contextmanager
+def attr_context(*args, **attrs):
+    
+    for arg in args:
+        attrs.update(arg)
+    
+    existing = {}
+    try:
+        for name, value in attrs.iteritems():
+            existing[name] = cmds.getAttr(name)
+            cmds.setAttr(name, value)
+    except:
+        for name, value in existing.iteritems():
+            cmds.setAttr(name, value)
+        raise
+    
+    yield existing.copy()
+    
+    for name, value in existing.iteritems():
+        cmds.setAttr(name, value)
+
+
+playblast_attrs = {
+    'defaultRenderGlobals.imageFormat': 8,
+    'defaultResolution.width': 1280,
+    'defaultResolution.height': 720,
+    'defaultResolution.deviceAspectRatio': 1.777,
+    'defaultResolution.pixelAspect': 1.0,
+    'defaultResolution.dotsPerInch': 72,
+    'defaultResolution.pixelDensityUnits': 0,
+}
+
+
+def playblast(**kwargs):
+    
+    current_panel = cmds.getPanel(withFocus=True)
+    panel_type = cmds.getPanel(typeOf=current_panel) 
+    if panel_type == 'modelPanel':
+        cam = cmds.modelPanel(current_panel, q=True, camera=True)
+    else:
+        cam = None
+        cmds.warning('Select a modeling panel to playblast')
+    
+    if cam:
+        displayFilmGate = cmds.camera(cam, q=True, displayFilmGate=True)
+        displayResolution = cmds.camera(cam, q=True, displayResolution=True)
+        overscan = cmds.camera(cam, q=True, overscan=True)
+        camera_attrs = {
+            cam + '.horizontalFilmOffset': 0,
+            cam + '.verticalFilmOffset': 0,
+        }
+    else:
+        camera_attrs = {}
+    
+    unit = cmds.currentUnit(q=True, linear=True)
+    
+    try:
+        if cam:
+            cmds.camera(cam, edit=True,
+                displayFilmGate=0,
+                displayResolution=1,
+                overscan=1,
+            )
+        cmds.currentUnit(linear='cm')
+        with attr_context(playblast_attrs, camera_attrs):
+            cmds.playblast(**kwargs)
+        return True
+    finally:
+        if cam:
+            cmds.camera(cam, edit=True,
+                displayFilmGate=displayFilmGate,
+                displayResolution=displayResolution,
+                overscan=overscan,
+            )
+        cmds.currentUnit(linear=unit)
+    
+    
 class Widget(Base):
     
     def _setup_ui(self):
@@ -18,6 +97,7 @@ class Widget(Base):
         super(Widget, self)._setup_ui()
         
         self._playblast = QtGui.QPushButton(ui_utils.icon('silk/pictures', size=12, as_icon=True), "Playblast")
+        self._playblast.clicked.connect(self._on_playblast)
         self._movie_layout.addWidget(self._playblast)
         self._playblast.setFixedHeight(self._movie_path.sizeHint().height())
         self._playblast.setFixedWidth(self._playblast.sizeHint().width() + 2)
@@ -29,21 +109,16 @@ class Widget(Base):
         
         # Playblast the first screenshot.
         path = tempfile.NamedTemporaryFile(suffix=".jpg", prefix="publish", delete=False).name
-        image_format = cmds.getAttr('defaultRenderGlobals.imageFormat')
-        cmds.setAttr('defaultRenderGlobals.imageFormat', 8)
-        try:
-            frame = cmds.currentTime(q=True)
-            cmds.playblast(
-                frame=[frame],
-                format='image',
-                completeFilename=path,
-                viewer=False,
-                p=100,
-                framePadding=4,
-            )
-        finally:
-            cmds.setAttr('defaultRenderGlobals.imageFormat', image_format)
-        self.setThumbnail(path)
+        frame = cmds.currentTime(q=True)
+        if playblast(
+            frame=[frame],
+            format='image',
+            completeFilename=path,
+            viewer=False,
+            p=100,
+            framePadding=4,
+        ):
+            self.setThumbnail(path)
     
     def _on_movie_browse(self):
         
@@ -59,4 +134,30 @@ class Widget(Base):
         
         if res:
             self._movie_path.setText(res[0])
+    
+    def _on_playblast(self):
+
+        minTime = cmds.playbackOptions(q=True, minTime=True)
+        maxTime = cmds.playbackOptions(q=True, maxTime=True)
+        frame_rate = cmds.playbackOptions(q=True, framesPerSecond=True)
+        
+        directory = tempfile.mkdtemp('playblast.')
+        playblast(
+            startTime=minTime,
+            endTime=maxTime,
+            format='image',
+            viewer=False,
+            p=100,
+            framePadding=4,
+            filename=directory + '/frame',
+        )
+        
+        # Playblasting puts us behind the main window.
+        self.raise_()
+        
+        # Eventually wait for this to close.
+        proc = subprocess.Popen(['mplay', '-T', '-R', '-r', str(int(frame_rate)), directory + '/frame.$F4.jpg'])
+        
+        self._movie_path.setText(directory + '/frame.####.jpg')
+        
         
