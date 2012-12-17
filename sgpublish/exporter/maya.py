8b6b8e48ba26bc3d96e2fb57106e638f3d996cc6
Mike Boers <mboers@keypics.com>
2012-12-17 12:05:16 -0800

    Move QuickTime making to Maya exporter

--- sgpublish/exporter/maya.py
+++ sgpublish/exporter/maya.py
@@ -1,10 +1,14 @@
 from __future__ import absolute_import
 
 import os
-
-from . import base
+import datetime
 
 from maya import cmds, mel
+
+import uifutures
+
+from . import base
+from sgpublish import utils
 
 
 maya_version = int(mel.eval('about -version').split()[0])
@@ -69,6 +73,8 @@ class Exporter(base.Exporter):
         cmds.fileInfo('sgpublish_%s_ids' % self.publish_type, ','.join(str(x) for x in sorted(ids)))
     
     def before_export_publish(self, publisher, **kwargs):
+
+        # Add a bunch of metadata.
         publisher.metadata['maya'] = {
             'version': maya_version,
             'references': [str(x) for x in cmds.file(query=True, reference=True) or []],
@@ -76,6 +82,58 @@ class Exporter(base.Exporter):
             'min_time': cmds.playbackOptions(query=True, minTime=True),
             'max_time': cmds.playbackOptions(query=True, maxTime=True),
         }
+
+        # Playblasts should be converted into frames.
+        if publisher.frames_path and not publisher.movie_path:
+        
+            # Put it in the dailies folder.
+            # TODO: Do this with SGFS templates.
+            project_root = publisher.sgfs.path_for_entity(publisher.link.project())
+            movie_directory = os.path.join(
+                project_root,
+                'VFX_Dailies',
+                datetime.datetime.now().strftime('%Y-%m-%d'),
+                publisher.link.fetch('step.Step.code') or 'Unknown',
+            )
+            movie_path = os.path.join(
+                movie_directory,
+                publisher.name + '_v%04d.mov' % publisher.version,
+            )
+        
+            # Make it unique.
+            if os.path.exists(movie_path):
+                base, ext = os.path.splitext(movie_path)
+                for i in itertools.count(1):
+                    movie_path = '%s_%04d%s' % (base, i, ext)
+                    if not os.path.exists(movie_path):
+                        break
+            
+            # Make the folder.
+            if not os.path.exists(movie_directory):
+                os.makedirs(movie_directory)
+            
+            sound_path = get_sound_for_frames(publisher.frames_path) or get_current_sound()
+            
+            # Spawn the job.
+            print '# Scheduling make_quicktime to %r from %r' % (movie_path, publisher.frames_path)
+            if sound_path:
+                print '# Sound from %r' % sound_path
+            
+            with uifutures.Executor() as executor:
+                
+                executor.submit_ext(
+                    func=utils.make_quicktime,
+                    args=(movie_path, publisher.frames_path, sound_path),
+                    name="QuickTime \"%s_v%04d\"" % (publisher.name, publisher.version),
+                )
+            
+            # Finally set the Shotgun attributes.
+            publisher.movie_path = movie_path
+            publisher.movie_url = {
+                'url': 'http://keyweb' + movie_path,
+                'name': os.path.basename(movie_path),
+            }
+            publisher.frames_path = None
     
     def fields_for_review_version(self, publisher, **kwargs):
         min_time = cmds.playbackOptions(query=True, minTime=True)
--- sgpublish/mayatools/publish_scene.py
+++ sgpublish/mayatools/publish_scene.py
@@ -20,14 +20,12 @@ Qt = QtCore.Qt
 
 from maya import cmds
 
-import uifutures
 from sgfs import SGFS
 
 from sgpublish import uiutils as ui_utils
 from sgpublish import utils
 from sgpublish.exporter import maya as io_maya
 from sgpublish.exporter.ui.publish import maya as ui_publish
-from sgpublish.exporter.maya import get_sound_for_frames, get_current_sound
 
 
 def basename(src_path=None):    
@@ -62,58 +60,6 @@ class SceneExporter(io_maya.Exporter):
         # Set the primary path.
         publisher.path = dst_path
         
-        # Playblasts should be converted into frames.
-        if publisher.frames_path and not publisher.movie_path:
-        
-            # Put it in the dailies folder.
-            # TODO: Do this with SGFS templates.
-            project_root = publisher.sgfs.path_for_entity(publisher.link.project())
-            movie_directory = os.path.join(
-                project_root,
-                'VFX_Dailies',
-                datetime.datetime.now().strftime('%Y-%m-%d'),
-                publisher.link.fetch('step.Step.code') or 'Unknown',
-            )
-            movie_path = os.path.join(
-                movie_directory,
-                publisher.name + '_v%04d.mov' % publisher.version,
-            )
-        
-            # Make it unique.
-            if os.path.exists(movie_path):
-                base, ext = os.path.splitext(movie_path)
-                for i in itertools.count(1):
-                    movie_path = '%s_%04d%s' % (base, i, ext)
-                    if not os.path.exists(movie_path):
-                        break
-            
-            # Make the folder.
-            if not os.path.exists(movie_directory):
-                os.makedirs(movie_directory)
-            
-            sound_path = get_sound_for_frames(publisher.frames_path) or get_current_sound()
-            
-            # Spawn the job.
-            print '# Scheduling make_quicktime to %r from %r' % (movie_path, publisher.frames_path)
-            if sound_path:
-                print '# Sound from %r' % sound_path
-            
-            with uifutures.Executor() as executor:
-                
-                executor.submit_ext(
-                    func=utils.make_quicktime,
-                    args=(movie_path, publisher.frames_path, sound_path),
-                    name="QuickTime \"%s_v%04d\"" % (publisher.name, publisher.version),
-                )
-                
-            
-            # Finally set the Shotgun attributes.
-            publisher.movie_path = movie_path
-            publisher.movie_url = {
-                'url': 'http://keyweb' + movie_path,
-                'name': os.path.basename(movie_path),
-            }
-            publisher.frames_path = None
         
 class PublishWidget(ui_publish.Widget):
     
